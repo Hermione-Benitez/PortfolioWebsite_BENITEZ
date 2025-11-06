72e0687fff031127730a8057267ea3fa9c409750
benitezhermione@gmail.com <159205348+Yuniiii25@users.noreply.github.com>
2025-11-07 02:17:21 +0800

    update

--- PortfolioWebsite_BENITEZ/Controllers/HomeController.cs
+++ PortfolioWebsite_BENITEZ/Controllers/HomeController.cs
@@ -21,6 +21,16 @@ namespace PortfolioWebsite_BENITEZ.Controllers
             return View();
         }
 
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
